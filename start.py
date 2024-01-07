bb44787df0be9f679c4a3742f25a8a08dea62254
Jeremi971 <79988469+Jeremi971@users.noreply.github.com>
2024-01-07 18:53:55 +0100

    info start.py

--- start.py
+++ start.py
@@ -7,3 +7,8 @@ from V.visual import startup
 # Démarrage du programme
 startup()
 
+#Mode d'emploi du script de poeme
+#1erment start.py
+#la page option s'affiche
+#Je  peux afficher les options avec show_dom
+#La syntaxe pour afficher une option est DOM=Option 
